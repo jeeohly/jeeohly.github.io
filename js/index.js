59bc74a8ddf6e1469af00cc9a7c6a8db0d3ad7e3
Gio Lee <giolee8@gmail.com>
2022-08-12 12:06:00 -0400

    added random response list

--- js/index.js
+++ js/index.js
@@ -2,6 +2,23 @@ $( document ).ready(function() {
     
 });
 
+var responses = ["Ask Jarod", "I don't know", "lolzcatz", "epic face"]
+
+function getRandomResponse(responses) {
+    return responses[Math.floor(Math.random() * responses.length)];  
+}
+
 $("#send").click(function(){
-    $("#response").html($("#message").val());
+    $("#responses").append("<li class='list-group-item' style='text-align:right;'>" + $("#message").val() + "</li>");
+    $("#responses").append("<li class='list-group-item'>Giobot: " + getRandomResponse(responses) + "</li>");
+    $("#message").val("");
+    if($("#responses li").length > 20){
+        $("#responses > li").slice(0, 2).remove();
+    }
 }); 
+
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        $("#send").click()
+    }
+});
