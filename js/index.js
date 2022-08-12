33fdab1c734dafccd28f5f4d7456aed3bc9defd2
Gio Lee <giolee8@gmail.com>
2022-08-11 21:16:38 -0400

    Testing out socket.io

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+});
+
+socket.emit('createMessage', {
+    from: 'Gio',
+    text: 'Hi'
+}, function (data) {
+    console.log('Got it', data)
+});
