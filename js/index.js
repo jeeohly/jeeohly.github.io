8bf9875ca2007325db697d6530e6a6eef2622243
Gio Lee <giolee8@gmail.com>
2022-08-13 13:30:09 -0400

    random jokes

--- js/index.js
+++ js/index.js
@@ -2,21 +2,30 @@ $( document ).ready(function() {
     
 });
 
-var responses = ["Ask Jarod", "I don't know", "lolzcatz", "epic face"]
-
 function getRandomResponse(responses) {
     return responses[Math.floor(Math.random() * responses.length)];  
 }
 
 $("#send").click(function(){
-    if($("#message").val() != ""){
-        $("#responses").append("<span class='float-end'><div class='alert alert-primary float-end'>" + $("#message").val() + "</span></div></span>");
-        $("#responses").append("<span class='float-start'><div class='alert alert-secondary float-start'>Giobot: " + getRandomResponse(responses) + "</div></span>");
-    }
-    if($("#responses div").length > 20){
-        $("#responses > ").slice(0, 2).remove();
-    }
-    $("#responses").scrollTop($("#responses")[0].scrollHeight);
+    $.ajax({
+        url: "https://icanhazdadjoke.com/",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            if($("#message").val() != ""){
+                $("#responses").append("<span class='float-end'><div class='alert alert-primary float-end'>" + $("#message").val() + "</span></div></span>");
+                $("#responses").append("<span class='float-start'><div class='alert alert-secondary float-start'>Giobot: " + data.joke + "</div></span>");
+                $("#message").val("");
+            }
+            if($("#responses div").length > 20){
+                $("#responses > ").slice(0, 2).remove();
+            }
+            $("#responses").scrollTop($("#responses")[0].scrollHeight);
+        },
+        error: function(){
+          console.log("Cannot get dad joke.");
+        }
+    });
 }); 
 
 $(document).on('keypress',function(e) {
